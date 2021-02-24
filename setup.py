64fd4064118157093c067832b70f7d40b3e634e1
Besian I. Sejdiu <besiansejdiu@gmail.com>
2021-02-24 00:30:54 -0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='prolint',
+    #packages=['prolint'],
+    version='0.7.0',
+    description='Lipid-protein analysis',
+    author='Besian I. Sejdiu',
+    license='MIT',
+    python_requires='>=3.6',
+    packages=find_packages(),
+    install_requires=['numpy==1.15.4', 'pandas==0.24.0', 'scipy', 'mdtraj==1.9.2', 'pyyaml', 'colorcet', 'bokeh==1.4.0', 'networkx', 'jupyterlab', 'nglview==2.7.7'],
+)
+
